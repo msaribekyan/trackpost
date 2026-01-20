3cea4aef0f7f876adf2ff734bc8018ea0de8324d
Mher Saribekyan <saribekyanmher@gmail.com>
2026-01-20 20:08:08 +0400

    idk what im doing

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,18 @@ use std::process::exit;
 mod request;
 use request::request::request;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let tracking: [(&str, &str); _] = [
         ("Package 1", "UB848893366LV"),
         ("Package 2", "UB849021351LV"),
     ];
 
-    let mut handles = vec![];
     for track in tracking {
-        let handle = tokio::spawn(async move {
-            request(track.0.to_string(), track.1.to_string()).await
-        });
-        handles.push(handle)
+        let resp = match request(track.1) {
+            Ok(r) => println!("{r}"),
+            Err(e) => println!("Error: {e}"),
+        };
     }
 
-    for handle in handles {
-        let result = handle.await;
-
-        match result {
-            Ok(value) => println!("{value}"),
-            Err(e) => eprintln!("{e}"),
-        }
-    }
     exit(0);
 }
--- src/request.rs
+++ src/request.rs
@@ -1,9 +1,8 @@
 pub mod request {
     #[derive(serde::Deserialize)]
-    struct Result
-    {
+    struct Location {
         #[allow(dead_code)]
-        #[serde(rename="local_Date")]
+        #[serde(rename = "local_Date")]
         local_date: String,
         #[allow(dead_code)]
         country: String,
@@ -12,69 +11,72 @@ pub mod request {
         #[allow(dead_code)]
         category: String,
         #[allow(dead_code)]
-        #[serde(rename="nextOffice")]
+        #[serde(rename = "nextOffice")]
         next_office: String,
         #[allow(dead_code)]
-        #[serde(rename="extraInformation")]
+        #[serde(rename = "extraInformation")]
         extra_information: String,
         #[allow(dead_code)]
-        #[serde(rename="eventDate")]
+        #[serde(rename = "eventDate")]
         event_date: String,
     }
 
     #[derive(serde::Deserialize)]
-    struct Data
-    {
-        result: Vec<Result>,
+    struct Data {
+        result: Vec<Location>,
         #[allow(dead_code)]
-        #[serde(rename="estimatedDelivery")]
+        #[serde(rename = "estimatedDelivery")]
         estimated_delivery: Option<String>,
         #[allow(dead_code)]
-        #[serde(rename="isEMS")]
+        #[serde(rename = "isEMS")]
         is_ems: bool,
         delivered: bool,
     }
 
     #[derive(serde::Deserialize)]
-    struct Package
-    {
+    struct Package {
         error: bool,
         message: Option<String>,
         data: Data,
     }
 
-    pub async fn request(name: String, tracking_number: String) -> String {
-        let mut out_string = format!("{name} ({tracking_number}): ");
-        let client = reqwest::Client::new();
-        let url = format!("https://api.haypost.am/trackingNumber/?trackingNumber={}&lng=en", tracking_number);
-        match client.get(url).send().await {
-            Ok(result) => {
-                let json_str = result.text().await.ok().unwrap();
-                match serde_json::from_str::<Package>(&json_str) {
-                    Ok(package) => {
-                        if package.error {
-                            out_string.push_str(&format!("{}", package.message.unwrap()))
-                        }
-                        else
-                        {
-                            if package.data.delivered {
-                               out_string.push_str(&format!("Package delilvered"));
-                            }
-                            else
-                            {
-                                let result = package.data.result;
-                                let latest_result = &result[result.len() - 1];
-                                out_string.push_str(&format!("{} - {}", latest_result.location, latest_result.event))
-                            }
-                        }
-                    },
-                    Err(e) => {
-                        out_string.push_str(&format!("JSON error: {:#?}", e));
-                    },
-                }
-            },
-           Err(e) => out_string.push_str(&format!("Fetching error: {:#?}", e)),
+    pub fn request(tracking_number: &str) -> Result<String, String> {
+        let client = reqwest::blocking::Client::new();
+        let url = format!(
+            "https://api.haypost.am/trackingNumber/?trackingNumber={tracking_number}&lng=en"
+        );
+        let result = match client.get(url).send() {
+            Ok(res) => res,
+            Err(e) => return Err(format!("Fetching error: {:#?}", e)),
+        };
+
+        let json_str = match result.text() {
+            Ok(json_str) => json_str,
+            Err(e) => return Err(format!("Something error: {:#?}", e)),
+        };
+
+        let package = match serde_json::from_str::<Package>(&json_str) {
+            Ok(package) => package,
+            Err(e) => return Err(format!("JSON error: {:#?}", e)),
+        };
+
+        if package.error {
+            match package.message {
+                Some(message) => return Err(format!("{}", message)),
+                None => return Err(format!("Package error with no message.")),
+            }
         }
-        out_string
+
+        if package.data.delivered {
+            return Ok("Package delivered!".to_string());
+        }
+
+        let result = package.data.result;
+        if result.len() == 0 {
+            return Err(format!("No info."));
+        }
+
+        let latest_result = &result[result.len() - 1];
+        return Ok(format!("{} - {}", latest_result.location, latest_result.event));
     }
 }
